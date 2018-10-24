609beffc07226fa65ee266c76868f1fe686d8924
RyanAbraham <ryan_ja@live.ca>
2018-10-24 08:39:12 -0400

    Add basic functionality for gameplay simulation

--- simulators/game.go
+++ simulators/game.go
@@ -4,14 +4,66 @@ import (
 	"github.com/ryanabraham/urserver/models"
 )
 
+// TODO: Possibly abstract this to its own model
 type gameState struct {
-	hands  [2]models.CardZone
-	fields [2]models.CardZone
-	decks  [2]models.Deck
+	hands     [2]models.CardZone
+	fields    [2]models.CardZone
+	decks     [2]models.Deck
+	playerHPs [2]int
+	turn      int
 }
 
 var state gameState
-var turn int
 
-func startGame() {
+// PlayGame simulates a game with 2 decks.
+// Returns 1 for player 1 win, 2 for player 2, and 0 for a draw.
+func PlayGame(d1, d2 models.Deck) int {
+	startGame(d1, d2)
+	for {
+		playTurn()
+		// Check if a player won through damage
+		for idx, hp := range state.playerHPs {
+			if hp <= 0 {
+				return idx + 1
+			}
+		}
+	}
+	return 0
+}
+
+func playTurn() {
+	p := state.turn   // Player turn
+	ep := (p + 1) % 2 // Enemy turn
+
+	// Draw a card
+	state.hands[p].AddCard(state.decks[p].DrawCard())
+
+	// Reduce clocks
+	for _, c := range state.hands[p].Cards {
+		if c.ClockDown() {
+			state.fields[p].AddCard(c)
+			state.hands[p].RemoveCard(c)
+		}
+	}
+
+	// Declare attacks
+	for idx, c := range state.fields[p].Cards {
+		if len(state.fields[ep] >= idx {
+			// There is an enemy card blocking this attack
+			state.fields[ep].Cards[idx].Damage(c.Pow)
+		} else {
+			state.playerHPs[ep] -= c.Pow
+		}
+	}
+	
+	// End turn
+	state.turn = ep
+}
+
+func startGame(d1, d2 models.Deck) {
+	state.decks = [...]models.Deck{d1, d2}
+	for _, deck := range state.decks {
+		deck.Shuffle()
+	}
+	state.turn = 0
 }
