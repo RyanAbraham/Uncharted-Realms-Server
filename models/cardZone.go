d4a6bb6a1492722bdc9153f4544f5647dd7ebd80
RyanAbraham <ryan_ja@live.ca>
2018-10-24 08:38:49 -0400

    Fix some unexported types

--- models/cardZone.go
+++ models/cardZone.go
@@ -6,21 +6,21 @@ import (
 
 // CardZone holds a list of cards in that zone
 type CardZone struct {
-	cards []Card
+	Cards []Card
 }
 
 // AddCard adds a given card to the CardZone
 func (cz *CardZone) AddCard(c Card) {
-	cz.cards = append(cz.cards, c)
+	cz.Cards = append(cz.Cards, c)
 }
 
 // RemoveCard removes a given card from the CardZone
 func (cz *CardZone) RemoveCard(c Card) {
 	if i := cz.getIndexOfCard(c); i != -1 {
 		// From https://github.com/golang/go/wiki/SliceTricks delete
-		copy(cz.cards[i:], cz.cards[i+1:])
-		cz.cards[len(cz.cards)-1] = Card{}
-		cz.cards = cz.cards[:len(cz.cards)-1]
+		copy(cz.Cards[i:], cz.Cards[i+1:])
+		cz.Cards[len(cz.Cards)-1] = Card{}
+		cz.Cards = cz.Cards[:len(cz.Cards)-1]
 	}
 }
 
--- models/deck.go
+++ models/deck.go
@@ -8,21 +8,21 @@ type Deck struct {
 	cards []Card
 }
 
-func (d *Deck) drawCard() Card {
+func (d *Deck) DrawCard() Card {
 	var c Card
 	c, d.cards = d.cards[0], d.cards[1:]
 	return c
 }
 
-func (d *Deck) drawCards(num int) []Card {
+func (d *Deck) DrawCards(num int) []Card {
 	var cards []Card
 	for x := 0; x < num; x++ {
-		cards = append(cards, d.drawCard())
+		cards = append(cards, d.DrawCard())
 	}
 	return cards
 }
 
-func (d *Deck) shuffle() {
+func (d *Deck) Shuffle() {
 	rand.Shuffle(len(d.cards), func(i, j int) {
 		d.cards[i], d.cards[j] = d.cards[j], d.cards[i]
 	})
