17f966e3d8e5ab20d04b4dc9673468184d9cc443
RyanAbraham <ryan_ja@live.ca>
2018-10-27 11:39:26 -0400

    Add stronger card test

--- simulators/game_test.go
+++ simulators/game_test.go
@@ -8,7 +8,9 @@ import (
 )
 
 var one = helpers.FakeCard(1, 1, 1, nil)
+var two = helpers.FakeCard(2, 2, 2, nil)
 var oneDeck = helpers.DeckOf(one, 10)
+var twoDeck = helpers.DeckOf(two, 10)
 var emptyDeck = models.Deck{}
 
 func TestGuaranteedWins(t *testing.T) {
@@ -22,12 +24,22 @@ func TestGuaranteedWins(t *testing.T) {
 	}
 }
 func TestMoreCardsWins(t *testing.T) {
-	oneMinus := helpers.DeckOf(one, 9)
-	result := Play(oneDeck, oneMinus)
+	oneMinusDeck := helpers.DeckOf(one, 9)
+	result := Play(oneDeck, oneMinusDeck)
 	if result != 0 {
 		t.Errorf("Game result incorrect, got: %d, expected %d.", result, 0)
 	}
-	result = Play(oneMinus, oneDeck)
+	result = Play(oneMinusDeck, oneDeck)
+	if result != 1 {
+		t.Errorf("Game result incorrect, got: %d, expected %d.", result, 1)
+	}
+}
+func TestStrongerCardsWins(t *testing.T) {
+	result := Play(twoDeck, oneDeck)
+	if result != 0 {
+		t.Errorf("Game result incorrect, got: %d, expected %d.", result, 0)
+	}
+	result = Play(oneDeck, twoDeck)
 	if result != 1 {
 		t.Errorf("Game result incorrect, got: %d, expected %d.", result, 1)
 	}
