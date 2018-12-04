8a47097d78e2ea242e4c22f041e21087b6c39d56
RyanAbraham <ryan_ja@live.ca>
2018-12-04 17:59:18 -0500

    Add CORS

--- handlers/app.go
+++ handlers/app.go
@@ -52,6 +52,12 @@ func RespondWithError(w http.ResponseWriter, code int, msg string) {
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
+	enableCors(&w)
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+// enableCors is a temporary testing function that allows easy localhost testing
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
