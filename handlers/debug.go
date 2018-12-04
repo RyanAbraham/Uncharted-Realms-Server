072138fec0a22eb11ad661057b8cf4a3b68a2505
RyanAbraham <ryan_ja@live.ca>
2018-12-04 17:48:09 -0500

    Send decklists in debug response

--- handlers/debug.go
+++ handlers/debug.go
@@ -4,8 +4,16 @@ import (
 	"net/http"
 
 	"github.com/ryanabraham/urserver/helpers"
+	"github.com/ryanabraham/urserver/models"
 	"github.com/ryanabraham/urserver/simulators"
 )
+
+type debugResponse struct {
+	Winner       int                  `json:"winner"`
+	AnimationLog *models.AnimationLog `json:"animationLog"`
+	Seed         int64                `json:"seed"`
+	Decks        []models.Deck        `json:"decks"`
+}
 
 // DebugHandler serves a sample JSON response for a game
 func (a *App) DebugHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +22,11 @@ func (a *App) DebugHandler(w http.ResponseWriter, r *http.Request) {
 	var oneDeck = helpers.DeckOf(one, 10)
 	var twoDeck = helpers.DeckOf(two, 10)
 	result := simulators.Play(twoDeck, oneDeck)
-	RespondWithJSON(w, 200, result)
+	response := debugResponse{
+		Winner:       result.Winner,
+		AnimationLog: result.AnimationLog,
+		Seed:         result.Seed,
+		Decks:        []models.Deck{oneDeck, twoDeck},
+	}
+	RespondWithJSON(w, 200, response)
 }
