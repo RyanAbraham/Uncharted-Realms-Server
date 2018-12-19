ce91407829495ba65dc64646dc20836706177610
RyanAbraham <ryan_ja@live.ca>
2018-12-19 08:52:43 -0500

    Add drawCard test

--- simulators/game_test.go
+++ simulators/game_test.go
@@ -1,6 +1,8 @@
 package simulators
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/ryanabraham/urserver/helpers"
@@ -51,5 +53,22 @@ func TestEqualDeckWinsOnDraw(t *testing.T) {
 	result := Play(oneDeck, oneDeck)
 	if result.Winner != 1 {
 		t.Errorf("Game result incorrect, got: %d, expected %d.", result, 1)
+	}
+}
+
+func TestDrawCard(t *testing.T) {
+	state := gameState{
+		decks: [2]models.Deck{
+			models.Deck{Cards: []models.Card{one}},
+			models.Deck{},
+		},
+	}
+	expectedState := state
+	expectedState.decks[0].Cards = []models.Card{}
+	expectedState.hands[0].Cards = []*models.Card{&one}
+	drawCard(&state, 0)
+	fmt.Println(state.hands[0])
+	if !reflect.DeepEqual(state, expectedState) {
+		t.Errorf("Game state incorrect, got: %+v, expected %+v.", state, expectedState)
 	}
 }
