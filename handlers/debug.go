90c8f44810592534e48e1a48a56d1ec2a11decdc
RyanAbraham <ryan_ja@live.ca>
2018-12-20 08:57:44 -0500

    Fix deck type for debug route

--- handlers/debug.go
+++ handlers/debug.go
@@ -12,21 +12,19 @@ type debugResponse struct {
 	Winner       int                  `json:"winner"`
 	AnimationLog *models.AnimationLog `json:"animationLog"`
 	Seed         int64                `json:"seed"`
-	Decks        [][]models.Card      `json:"decks"`
+	Decks        []models.Deck        `json:"decks"`
 }
 
 // DebugHandler serves a sample JSON response for a game
 func (a *App) DebugHandler(w http.ResponseWriter, r *http.Request) {
-	var one = helpers.FakeCard(1, 1, 1, nil)
-	var two = helpers.FakeCard(2, 2, 2, nil)
-	var oneDeck = helpers.DeckOf(one, 10)
-	var twoDeck = helpers.DeckOf(two, 10)
+	var oneDeck = helpers.DeckOf(helpers.FakeCard(1, 1, 1, nil), 10)
+	var twoDeck = helpers.DeckOf(helpers.FakeCard(2, 2, 2, nil), 10)
 	result := simulators.Play(twoDeck, oneDeck)
 	response := debugResponse{
 		Winner:       result.Winner,
 		AnimationLog: result.AnimationLog,
 		Seed:         result.Seed,
-		Decks:        [][]models.Card{oneDeck.Cards, twoDeck.Cards},
+		Decks:        []models.Deck{oneDeck, twoDeck},
 	}
 	RespondWithJSON(w, 200, response)
 }
