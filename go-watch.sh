d40c33a7880f967459f1722816677d4e8480249e
Ryan Abraham <Ryan_JA@live.ca>
2019-01-09 23:42:50 -0500

    Add build-watch script

---
+++ go-watch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+while true; do
+  go build
+  $@ &
+  PID=$!
+  inotifywait -r -e modify --exclude '\.log' .
+  kill $PID
+  echo -e "${GREEN}Changes detected! Reloading...${NC}"
+done
